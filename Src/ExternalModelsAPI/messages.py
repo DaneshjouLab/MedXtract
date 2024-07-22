96de2eedc8df528244594f03415dcad80a230d51
Jacob Goldberg <jacobgoldbergredsox@gmail.com>
2024-07-22 13:19:18 -0700

    simplify message class

--- Src/ExternalModelsAPI/messages.py
+++ Src/ExternalModelsAPI/messages.py
@@ -8,17 +8,11 @@ class Message:
 
     # Currently a message only contains the content and sender information could add content type to future proof
     def __init__(self, **kwargs):
-        allowed_attributes = {'content': None, 'sender': None}
-        
-        for key, value in kwargs.items():
-            if key in allowed_attributes:
-                setattr(self, key, value)
-            else:
-                raise AttributeError(f"Invalid attribute: {key}")
-            
-        for key in allowed_attributes:
-            if not hasattr(self, key):
-                setattr(self, key, allowed_attributes[key])
+        if 'content' in kwargs and 'sender' in kwargs:
+            self.content = kwargs['content']
+            self.sender = kwargs['sender']
+        else:
+            raise AttributeError("Invalid input: must provide 'content' and 'sender' attributes")
 
     def getContent(self):
         return self.content
