3e4bd363aa61bfc887218b13fd74146c7256d55a
Jacob Goldberg <jacobgoldbergredsox@gmail.com>
2024-07-22 13:56:07 -0700

    unit test Prompt class

--- Src/ExternalModelsAPI/unittesting.py
+++ Src/ExternalModelsAPI/unittesting.py
@@ -1,5 +1,6 @@
 import unittest
 from messages import Message
+from promptconstructor import Prompt
 
 class TestMessage(unittest.TestCase):
     def test_proper_initialization(self):
@@ -19,6 +20,64 @@ class TestMessage(unittest.TestCase):
         message = Message()
         self.assertEqual(message.getContent(), None)
         self.assertEqual(message.getSender(), None)
+
+class TestPrompt(unittest.TestCase):
+    def test_proper_initialization(self):
+        prompt = Prompt()
+        self.assertEqual(len(prompt.getList()), 0)
+
+    def test_add(self):
+        prompt = Prompt()
+        content = "this is test add 1"
+        sender = "user"
+        message = Message(content = content, sender = sender)
+        prompt.add(message = message)
+        self.assertEqual(len(prompt.getList()), 1)
+        self.assertEqual(prompt.peek().getContent(), content)
+        self.assertEqual(prompt.peek().getSender(), sender)
+        content = "this is test add 2"
+        sender = "assistant"
+        prompt.add(content = content, sender = sender)
+        self.assertEqual(len(prompt.getList()), 2)
+        self.assertEqual(prompt.peek().getContent(), content)
+        self.assertEqual(prompt.peek().getSender(), sender)
+        prompt.add()
+        self.assertEqual(len(prompt.getList()), 3)
+        self.assertEqual(prompt.peek().getContent(), None)
+        self.assertEqual(prompt.peek().getSender(), None)
+    
+    def test_insert(self):
+        prompt = Prompt()
+        prompt.add(content = "this is test add 1", sender = "user")
+        prompt.add(content = "this is test add 2", sender = "assistant")
+        content = "this is test add 3"
+        sender = "user"
+        index = 0
+        prompt.insert(content = content, sender = sender, index = index)
+        self.assertEqual(len(prompt.getList()), 3)
+        self.assertEqual(prompt.getList()[index].getContent(), content)
+        self.assertEqual(prompt.getList()[index].getSender(), sender)
+        content = "this is test add 4"
+        sender = "assistant"
+        index = 2
+        message = Message(content = content, sender = sender)
+        prompt.insert(index = index, message = message)
+        self.assertEqual(len(prompt.getList()), 4)
+        self.assertEqual(prompt.getList()[index].getContent(), content)
+        self.assertEqual(prompt.getList()[index].getSender(), sender)
+        index = 0
+        prompt.insert(index = index)
+        self.assertEqual(len(prompt.getList()), 5)
+        self.assertEqual(prompt.getList()[index].getContent(), None)
+        self.assertEqual(prompt.getList()[index].getSender(), None)
+        content = "this is test add 6"
+        sender = "user"
+        index = 4
+        message = Message(content = content, sender = sender)
+        with self.assertRaises(AttributeError):
+            prompt.insert(message = message)
+        
+
 
 
 if __name__ == '__main__':
