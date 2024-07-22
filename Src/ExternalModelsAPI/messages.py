371bf0759e9d6e5b0702500608a1ba9a24c718e0
Jacob Goldberg <jacobgoldbergredsox@gmail.com>
2024-07-22 12:18:37 -0700

    set-up messages class

--- Src/ExternalModelsAPI/messages.py
+++ Src/ExternalModelsAPI/messages.py
@@ -5,12 +5,25 @@ class Messages:
     builds Messages for the api's
     
     """
-    def __init__(self):
-    
-        pass
+    def __init__(self, **kwargs):
+        allowed_attributes = {'content': None, 'sender': None}
+        
+        for key, value in kwargs.items():
+            if key in allowed_attributes:
+                setattr(self, key, value)
+            else:
+                raise AttributeError(f"Invalid attribute: {key}")
+            
+        for key in allowed_attributes:
+            if not hasattr(self, key):
+                setattr(self, key, allowed_attributes[key])
 
-    def buildMessage():
-        pass
+    def getContent(self):
+        return self.content
+    
+    def getSender(self):
+        return self.sender
+
     
 
 
