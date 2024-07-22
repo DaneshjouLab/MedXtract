e00144f73d3aa9cc7566e6d3fbc581a898ddf107
Aaron Fanous <aron7268@gmail.com>
2024-07-22 16:15:50 -0700

    push

--- Src/ExternalModelsAPI/messages.py
+++ Src/ExternalModelsAPI/messages.py
@@ -1,7 +1,7 @@
 # you have many data objects, but only 
 # the following 
 from abc import ABC
-
+from typing import un
 class TextContent:
     def __init__(self, text):
         self.type = "text"
